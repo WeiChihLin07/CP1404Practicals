b617a293a01c2b825594c77dcd2741f6b14ed78d
WeiChihLin <weichih.lin@my.jcu.edu.au>
2019-08-20 21:47:12 +1000

    Refactor password_check to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -4,11 +4,34 @@ doesn't meet a minimum length set by a variable. The program should then print a
 MINIMUM_LENGTH = 6
 
 
-def main():
+def password_check():
     password = input("Enter at least {} characters password: ".format(MINIMUM_LENGTH))
     while len(password) < MINIMUM_LENGTH:
         password = input("Enter at least {} characters password: ".format(MINIMUM_LENGTH))
     print('*' * len(password))
+
+
+# password_check()
+
+
+"""Password checker with functions"""
+
+
+def main():
+    password = get_password(MINIMUM_LENGTH)
+    print_asterisks(password)
+
+
+def get_password(minimum_length):
+    password = input("Enter at least {} characters password: ".format(minimum_length))
+    while len(password) < minimum_length:
+        print("Password does not meet the length requirement.")
+        password = input("Enter at least {} characters password: ".format(minimum_length))
+    return password
+
+
+def print_asterisks(sequence):
+    print('*' * len(sequence))
 
 
 main()
