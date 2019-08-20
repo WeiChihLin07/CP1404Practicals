e84069908b0f40487356342f229c2eb191629c9c
WeiChihLin <weichih.lin@my.jcu.edu.au>
2019-08-20 22:53:01 +1000

    Refactor broken_score program to use functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -13,4 +13,27 @@ def broken_score():
         print("Bad")
 
 
-broken_score()
+# broken_score()
+
+
+"""Use a function that takes in the score as a parameter and returns the result to be printed.
+The function should not print it; the main program should store and print the returned value."""
+
+
+def main():
+    score = float(input("Enter score: "))
+    print(determine_status(score))
+
+
+def determine_status(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Pass"
+    else:
+        return "Bad"
+
+
+main()
