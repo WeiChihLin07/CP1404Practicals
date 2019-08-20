aadc953268cb5b141e2317903b1306186053fdea
WeiChihLin <weichih.lin@my.jcu.edu.au>
2019-08-20 22:17:32 +1000

    Refactor temperature program to use functions

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -26,4 +26,36 @@ def temperature():
     print("Thank you.")
 
 
-temperature()
+# temperature()
+
+"""Refactor temperature program with functions."""
+
+
+def main():
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = convert_celsius_to_fahrenheit(celsius)
+            print("Result: {:.2f} F".format(fahrenheit))
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
+            print(" Result: {:.2f} C".format(celsius))
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_celsius_to_fahrenheit(celsius):
+    return celsius * 9.0 / 5 + 32
+
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    return 5 / 9 * (fahrenheit - 32)
+
+
+main()
