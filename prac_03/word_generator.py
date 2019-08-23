42a185b4cf65db7d233d0d020674f40728db1d92
Weichihlin07 <weichih.lin@my.jcu.edu.au>
2019-08-23 10:02:39 +1000

    Need to fix function

--- prac_03/word_generator.py
+++ prac_03/word_generator.py
@@ -10,12 +10,54 @@ import random
 VOWELS = "aeiou"
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
 
-word_format = "ccvcvvc"
-word = ""
-for kind in word_format:
-    if kind == "c":
-        word += random.choice(CONSONANTS)
-    else:
-        word += random.choice(VOWELS)
 
-print(word)
+# def word_generator():
+#     word_format = "ccvcvvc"
+#     word = ""
+#     for kind in word_format:
+#         if kind == "c":
+#             word += random.choice(CONSONANTS)
+#         else:
+#             word += random.choice(VOWELS)
+#
+#     print(word)
+
+
+# word_generator()
+
+"""Refactor word_generator to use function"""
+
+
+def main():
+    word_format = input("Enter VOWELS or CONSONANTS: ").lower()
+    while not is_valid_format(word_format):
+        print("Invalid word format!")
+        word_format = input("Enter VOWELS or CONSONANTS: ").lower()
+    print("You enter {}:".format(word_format))
+
+    word = ""
+    for kind in word_format:
+        if kind == "c":
+            word += random.choice(CONSONANTS)
+        else:
+            word += random.choice(VOWELS)
+    print(word)
+
+
+def is_valid_format(word_format):
+    """Determine if the provided word format is valid."""
+    if True:
+        return False
+    else:
+        return True
+
+
+main()
+
+
+
+
+
+
+
+
