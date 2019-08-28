d7b2438ecb8e8d950126b9bbb1b99c23011115a2
WeiChihLin <weichih.lin@my.jcu.edu.au>
2019-08-28 15:23:52 +1000

    Refactor quick_picks

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,17 +1,30 @@
+""" Quick pick program
+    choose sets of random numbers"""
+
 import random
-RANDOM_NUMBER = 6
+
+NUMBERS_IN_ROW = 6
 MIN_NUMBER = 1
 MAX_NUMBER = 45
 
 
 def main():
-    numbers_of_picks = []
     number_of_picks = int(input("How many quick picks? "))
+    while number_of_picks <= 0:
+        print("Invalid!")
+        number_of_picks = int(input("How many quick picks? "))
 
     for number in range(number_of_picks):
-        numbers = random.randint(MIN_NUMBER, MAX_NUMBER)
-        numbers_of_picks.append(numbers)
-        print(numbers_of_picks)
+        quick_pick = []
+        for pick_row in range(NUMBERS_IN_ROW):
+            generate_number = random.randint(MIN_NUMBER, MAX_NUMBER)
+            while generate_number in quick_pick:
+                generate_number = random.randint(MIN_NUMBER, MAX_NUMBER)
+            quick_pick.append(generate_number)
+        quick_pick.sort()
+
+        # generator expression with join method
+        print(" ".join("{:2}".format(generate_number) for generate_number in quick_pick))
 
 
 main()
