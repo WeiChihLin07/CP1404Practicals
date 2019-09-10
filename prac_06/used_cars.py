ac232d80d6d97599db20e8b026ef7a213e6820c6
WeiChihLin <weichih.lin@my.jcu.edu.au>
2019-09-10 21:32:19 +1000

    Add names to the constructors

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -6,7 +6,7 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("My car", 180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
@@ -15,7 +15,14 @@ def main():
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
 
-    limo =
+    # Modifications
+    print(sep="")
+    limo = Car("Limo", 100)
+    limo.add_fuel(20)
+    print("fuel =", limo.fuel)
+    limo.drive(115)
+    print("odo =", limo.odometer)
+    print(limo)
 
 
 main()
