825ea2220692d8178610e8619f3751d06338a241
WeiChihLin <weichih.lin@my.jcu.edu.au>
2019-09-10 21:30:17 +1000

    Add the __str__ method and name field to the Car class

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,13 +4,18 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
         self.odometer = 0
+
+    def __str__(self):
+        """Return a string format of a Car object."""
+        return "{}, fuel={}, odometer={}".format(self.name, self.fuel, self.odometer)
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
