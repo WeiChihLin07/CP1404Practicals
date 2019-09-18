0b77b2ea2c89e08d0862d9d63fe6c483949e7f85
WeiChihLin <weichih.lin@my.jcu.edu.au>
2019-09-18 13:56:10 +1000

    Refactor docstring commands for readability

--- prac_07/box_layout_demo.py
+++ prac_07/box_layout_demo.py
@@ -6,6 +6,9 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """Build the Kivy GUI
+        return: reference to the root kivy widget
+        """
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
