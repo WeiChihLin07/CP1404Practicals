99e2cf11dbb3e2f6e81a085103baf21231347946
WeiChihLin <weichih.lin@my.jcu.edu.au>
2019-09-18 13:52:29 +1000

    Add handle_greet and handle_event functions for extra functionality

--- prac_07/box_layout_demo.py
+++ prac_07/box_layout_demo.py
@@ -1,3 +1,5 @@
+"""Kivy box_layout_demo for CP1404/CP5632"""
+
 from kivy.app import App
 from kivy.lang import Builder
 
@@ -7,6 +9,16 @@ class BoxLayoutDemo(App):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
+
+    def handle_greet(self):
+        """Greet user with Hello + username"""
+        print('test')
+        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
+
+    def handle_event(self):
+        """Clear the Text Input field and reset the output_label field"""
+        self.root.ids.input_name.text = ""
+        self.root.ids.output_label.text = "Enter your name"
 
 
 BoxLayoutDemo().run()
