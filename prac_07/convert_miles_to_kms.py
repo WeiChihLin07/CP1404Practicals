be89dac5098d8e41865e9633657a8a4a79ebb494
WeiChihLin <weichih.lin@my.jcu.edu.au>
2019-09-25 12:16:52 +1000

    Refactor program names

--- prac_07/convert_miles_to_kms.py
+++ prac_07/convert_miles_to_kms.py
@@ -16,7 +16,7 @@ class MilesConverterApp(App):
     def build(self):
         """Build the Kivy app from the kv file."""
         self.title = "Convert Miles to Kilometres"
-        self.root = Builder.load_file('convert_mile_km.kv')
+        self.root = Builder.load_file('convert_miles_to_kms.kv')
         return self.root
 
     def handle_calculate(self, text):
