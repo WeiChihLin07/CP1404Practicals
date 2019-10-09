186976e851dd95eca2217d558c2203c00833380c
WeiChihLin <weichih.lin@my.jcu.edu.au>
2019-10-09 13:00:25 +1000

    Refactor program to use round function in get_fare function

--- prac_08/taxi.py
+++ prac_08/taxi.py
@@ -20,7 +20,7 @@ class Taxi(Car):
 
     def get_fare(self):
         """Return the price for the taxi trip."""
-        return self.price_per_km * self.current_fare_distance
+        return round((self.price_per_km * self.current_fare_distance), 1)
 
     def start_fare(self):
         """Begin a new fare."""
