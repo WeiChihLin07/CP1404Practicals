915f9b1f20ff0a945158d546fd6c511a976594a3
WeiChihLin <weichih.lin@my.jcu.edu.au>
2019-11-19 14:30:22 +1000

    Refactor find_older program

--- ExamPrep/Functions/find_older.py
+++ ExamPrep/Functions/find_older.py
@@ -5,15 +5,26 @@ an integer threshold age value
 The function should return a list of names of the people who are older or equal to the threshold age
 """
 
-THRESHOLD_AGE = 34
 
-name_to_age = {"Bill": 21, "Jane": 34, "Steven": 56}
-
-print(find_older(name, age))
-
-
-# TODO
+def main():
+    names_to_ages = {"Bill": 21, "Jane": 34, "Sven": 56}
+    print(find_older(names_to_ages, 30))
+    print(find_elder(names_to_ages, 20))
 
 
-def find_older(name, age):
-    return [name for name, age in name_to_age.items() if age >= THRESHOLD_AGE]
+def find_older(names_to_ages, threshold_age):
+    """ for loop"""
+    older_people = []
+    for name in names_to_ages:
+        if names_to_ages[name] >= threshold_age:
+            older_people.append(name)
+    return older_people
+
+
+def find_elder(names_to_ages, threshold_age):
+    """list comprehension"""
+    elder_people = [name for name in names_to_ages if names_to_ages[name] >= threshold_age]
+    return elder_people
+
+
+main()
