8cf50afb21df592a3885070e12bdf1b596a0c713
WeiChihLin <weichih.lin@my.jcu.edu.au>
2019-11-19 14:47:48 +1000

    Refactor read_file program

--- ExamPrep/Loops/read_file.py
+++ ExamPrep/Loops/read_file.py
@@ -4,7 +4,5 @@ Write code to read the guitar.csv and print each part separately with the price 
 
 with open("guitar.csv", 'r') as in_file:
     for line in in_file:
-        guitar = line.strip(',')
-    print(guitar)
-
-# TODO
+        guitar = line.strip().split(',')
+        print("{:20} {:4} ${:.2f}".format(guitar[0], guitar[1], float(guitar[2])))
